3e10d04d3e5488ffce89e5942eb797ac80892c51
SuperAx <86346790+SuperAxFax@users.noreply.github.com>
2022-03-22 21:04:11 +0800

    Update UserDao.java

--- smbms_01/src/main/java/com/kuang/Dao/user/UserDao.java
+++ smbms_01/src/main/java/com/kuang/Dao/user/UserDao.java
@@ -11,4 +11,16 @@ public interface UserDao {
 
     //修改密码
     public int updatePwd(Connection connection, int id, String pwd) throws SQLException;
+
+    //根据用户名或者角色查询用户总数
+    public int getUserCount(Connection connection, String username, int userRole) throws SQLException;
+
+    //通过条件查询userList
+    public List<User> getUserList(Connection connection,String userName,int userRole,int currentPageNo,int pageSize) throws SQLException;
+
+    //增加用户
+    public int add(Connection connection, User user);
+
+    //判断用户编码与数据库中的编码是否一样
+    public User userCodeExist(Connection connection, String userCode) throws SQLException;
 }
