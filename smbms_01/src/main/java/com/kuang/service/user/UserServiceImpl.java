00fb8e2d13b1a990909f2d0e27cc59cd0d09308e
SuperAx <86346790+SuperAxFax@users.noreply.github.com>
2022-03-30 17:54:01 +0800

    Update UserServiceImpl.java

--- smbms_01/src/main/java/com/kuang/service/user/UserServiceImpl.java
+++ smbms_01/src/main/java/com/kuang/service/user/UserServiceImpl.java
@@ -53,6 +53,87 @@ public class UserServiceImpl implements UserService {
         return flag;
     }
 
+    //查询记录数
+    public int getUserCount(String username, int userRole) {
+        Connection connection = null;
+        int count = 0;
+        try {
+            connection = BaseDao.getConnection();
+            count = userDao.getUserCount(connection,username,userRole);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            BaseDao.closeResource(connection,null,null);
+        }
+        return count;
+    }
+
+    //根据条件查询用户列表
+    public List<User> getUserList(String queryUserName, int queryUserRole, int currentPageNo, int pageSize) {
+
+        List<User> userList = null;
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            userList = userDao.getUserList(connection, queryUserName, queryUserRole, currentPageNo, pageSize);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }finally{
+            BaseDao.closeResource(connection,null,null);
+        }
+        /*System.out.println("进入了service层");*/
+        return userList;
+    }
+
+    //添加用户
+    public boolean add(User user) {
+
+        Boolean flag = false;
+        Connection connection = null;
+        try {
+            connection = BaseDao.getConnection();
+            connection.setAutoCommit(false);//开启JDBC事务管理
+            int updateRows = userDao.add(connection, user);
+            connection.commit();
+
+            if (updateRows>0){
+                flag = true;
+                System.out.println("修改成功！");
+            }else {
+                System.out.println("修改失败！");
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+            try {
+                connection.rollback();//事务回滚
+            } catch (SQLException e1) {
+                e1.printStackTrace();
+            }
+        }finally {
+            BaseDao.closeResource(connection,null,null);
+        }
+        return flag;
+    }
+
+
+
+    //判断输入编码与用户编码是否相同
+
+    /*这个与ajax验证原密码不一样，那个由于密码已经放在了session中，直接从session中查找即可，而这个需要
+    在数据库中进行查找，看是否有相同的userCode。所以需要从前往后一套流程下来！*/
+    public User userCodeExist(String userCode) {
+        User user = null;
+        Connection connection = BaseDao.getConnection();
+        try {
+            user = userDao.userCodeExist(connection, userCode);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }finally {
+            BaseDao.closeResource(connection,null,null);
+        }
+         return user;
+    }
+
 
    /* @Test
    public void test(){
