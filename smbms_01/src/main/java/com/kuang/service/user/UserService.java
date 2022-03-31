d1f7c9f2f5787fed8b66942f22b51f0c19208282
SuperAx <86346790+SuperAxFax@users.noreply.github.com>
2022-03-31 20:42:38 +0800

    Update UserService.java

--- smbms_01/src/main/java/com/kuang/service/user/UserService.java
+++ smbms_01/src/main/java/com/kuang/service/user/UserService.java
@@ -8,5 +8,26 @@ public interface UserService {
 
     //根据用户id修改密码
     public boolean updatePwd(int id,String pwd);
+    
+    //查询记录数
+    public int getUserCount(String username, int userRole);
+
+    //根据条件查询用户列表
+    public List<User> getUserList(String queryUserName,int queryUserRole,int currentPageNo,int pageSize);
+
+    //增加用户
+    public boolean add(User user);
+
+    //判断用户编码是否与数据库中的重复
+    public User userCodeExist(String userCode);
+
+    //根据ID删除user
+    public boolean deleteUserById(Integer delId);
+    
+    //修改用户信息
+    public boolean modify(User user);
+
+    //根据Id查找User
+    public User getUserById(String id);
 }
 
